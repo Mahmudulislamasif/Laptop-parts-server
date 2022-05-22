bd160ad27636451d820f13a4175f623213d7f95a
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-22 18:51:35 +0600

    booking api add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port=process.env.PORT||5000
 const app=express()
@@ -21,11 +21,23 @@ async function run()
     try{
         await client.connect();
         const toolCollection=client.db('toolsList').collection('tools')
+        const bookingCollection=client.db('bookingList').collection('bookings')
         app.get('/tools',async(req,res)=>{
             const query={};
             const cursor=toolCollection.find(query)
             const tools=await cursor.toArray();
             res.send(tools);
+        })
+        app.get('/purchase/:id',async(req,res)=>{
+            const id=req.params.id
+            const query={_id:ObjectId(id)}
+            const purchase=await toolCollection.findOne(query)
+            res.send(purchase)
+        })
+        app.post('/booking',async(req,res)=>{
+            const newTool=req.body;
+            const result=await bookingCollection.insertOne(newTool);
+            res.send(result)
         })
     }
     finally
