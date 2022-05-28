55a051b21f223f9a7fc0b3486fbb7ac86c737ccb
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-28 23:44:28 +0600

    add api delete

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run()
         app.delete('/tools/:id',async(req,res)=>
         {
             const id=req.params.id;
+            console.log(id)
             const query={_id:ObjectId(id)};
             const result=await toolCollection.deleteOne(query)
             res.send(result);
