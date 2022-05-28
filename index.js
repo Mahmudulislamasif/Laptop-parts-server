bfe8d0631a4f7f0d6a8376d439758888d16af867
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-28 16:59:46 +0600

    add error fix

--- index.js
+++ index.js
@@ -7,22 +7,9 @@ const port=process.env.PORT||5000
 const app=express()
 
 //middleware
+app.use(cors({origin:"https://final-hero-project.web.app/"}))
 
-const corsConfig = {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("*", cors(corsConfig))
 app.use(express.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
-    next()
-})
-
-
 
 
 
