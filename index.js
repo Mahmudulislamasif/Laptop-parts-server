98d02aa57f5603b027bf45b521d94098cb2af74a
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-28 18:04:26 +0600

    add orgin as a firebase link

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const port=process.env.PORT||5000
 const app=express()
 
 //middleware
-
+app.use(cors({origin:"https://final-hero-project.web.app/"}))
 
 app.use(express.json())
-const corsConfig = {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("*", cors(corsConfig))
-app.use(express.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
-    next()
-})
+// const corsConfig = {
+//     origin: '*',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options("*", cors(corsConfig))
+// app.use(express.json())
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+//     next()
+// })
 
 // 
 
