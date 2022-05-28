e539189179911d8673602e43cf9faeb3911c3910
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-28 16:32:15 +0600

    add cors for error

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ const app=express()
 
 //middleware
 
-app.use(
-    cors({
-        origin:true,
-        optionsSuccessStatus:200,
-        credentials:true,
-    })
-);
-
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
 app.use(express.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+})
+
+
 
 
 
