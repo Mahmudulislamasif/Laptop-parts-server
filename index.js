2fa81c3f06ad7acde82d2b5b0922667c6ae1a68f
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-05-28 17:41:39 +0600

    heroku cors add

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ const port=process.env.PORT||5000
 const app=express()
 
 //middleware
-app.use(cors({origin:"https://final-hero-project.web.app/"}))
+
 
 app.use(express.json())
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+})
 
-
-
+// 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhtrr.mongodb.net/?retryWrites=true&w=majority`;
